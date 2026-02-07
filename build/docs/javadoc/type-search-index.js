c7a6adc0f8f86d8e9ed8aa0514dbf7c0421c577b
davider <69058122+davider0@users.noreply.github.com>
2026-02-07 15:24:20 +0100

    feat: mod works now

---
+++ build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.fumonomicon.plugin","l":"FumonomiconPlugin"},{"p":"org.fumonomicon.plugin","l":"HelpCommand"}];updateSearchResults();
